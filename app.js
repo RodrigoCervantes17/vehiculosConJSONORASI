e0de6c3519b3847a39227523e1514316c04ea600
RodrigoCervantes17 <99071743+RodrigoCervantes17@users.noreply.github.com>
2024-04-17 17:29:36 -0600

    Finalizacion de la API
    Se arregló todo gente, un saludo

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
-const express = require('express')
-const cors=require("cors")
-const app = express()
-app.use(cors())
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/vehiculos',(req,res)=>{
-    res.json({productos:productos});
-})
-app.get('/numeros', (req, res) => {
-  res.json({numeros:numeros, tipo:'listar'});
-})
+let vehiculos = [];
 
-app.get('/numeros/:id',(req,res)=>{
-    let num=parseInt(req.params.id);
-    indice=numeros.indexOf(num);
-    res.json({tipo:'busqueda',pos:indice});
-})
-app.get('/productos/:id',(req,res)=>{
-    res.json({tipo:'busqueda productos'});
-})
+app.get('/vehiculos', (req, res) => {
+  res.json({ vehiculos: vehiculos });
+});
 
-app.delete('/numeros/:id',(req,res)=>{
-    let num=parseInt(req.params.id);
-    let indice=numeros.indexOf(num);
-    numeros.splice(indice,1);
-    res.json({tipo:'delete'})
-})
+app.post('/vehiculos', (req, res) => {
+  let vehiculo = req.body.vehiculo;
+  vehiculos.push(vehiculo);
+  res.json({ tipo: 'agregar' });
+});
 
-app.post('/numeros',(req,res)=>{
-    let num=parseInt(req.body.num);
-    console.log(num);
-    let indice=numeros.indexOf(num);
-    if (indice==-1)
-        numeros.push(num);
-    res.json({tipo:'agregar'})
-})
+app.delete('/vehiculos/:placa', (req, res) => {
+  let placa = req.params.placa;
+  let indice = vehiculos.findIndex((v) => v.placa === placa);
+  if (indice !== -1) {
+    vehiculos.splice(indice, 1);
+    res.json({ tipo: 'eliminar' });
+  } else {
+    res.status(404).json({ error: 'No se encontró el vehículo' });
+  }
+});
 
-app.post('/productos',(req,res)=>{
-    let id=req.body.id;
-    //buscar el id, si no existe
-    let nombre=req.body.nombre;
-    let cantidad=req.body.cantidad;
-    let costo=req.body.costo;
-    let nuevo={id,nombre,cantidad,costo};
-    productos.push(nuevo);
-    res.json({tipo:'agregar'});
+app.get('/vehiculos/:placa', (req, res) => {
+  let placa = req.params.placa;
+  let vehiculo = vehiculos.find((v) => v.placa === placa);
+  if (vehiculo) {
+    res.json({ tipo: 'busqueda', vehiculo: vehiculo });
+  } else {
+    res.status(404).json({ error: 'No se encontró el vehículo' });
+  }
+});
 
-})
-app.listen(3000,()=>{console.log('Escuchando en puerto 3000')});
+app.listen(3000, () => {
+  console.log('Escuchando en puerto 3000');
+});
